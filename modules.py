2c61d72a659ea74c3913e5badddc467d577997c0
Micah Campbell <mxc3728@rit.edu>
2022-02-16 02:06:30 -0500

    1 click run by editing fortran

--- modules.py
+++ modules.py
@@ -32,7 +32,7 @@ def takeData(title_number, file, table_names, out_filename="Data.txt"):  # file 
                 title_line = count
                 title_taken = True
                 # print("Start is: ", table_names[title_number])
-            elif title_number+1 > len(table_names):
+            elif title_number + 1 > len(table_names):
                 end_line = len(file_content)
                 end_taken = True
             elif caps[0] == table_names[title_number + 1] and not end_taken:
@@ -53,30 +53,42 @@ def takeData(title_number, file, table_names, out_filename="Data.txt"):  # file 
     for i in data:
         if "E+00-" in i:
             i = i.replace("E+00-", "E+00 -")
-        output_file.write(str(i.strip()+"\n"))
+        output_file.write(str(i.strip() + "\n"))
     output_file.close()
-
     os.chdir(cwd)
 
 
-def atmos(planet_template):
+def atmos(planet_template, arg1="y", arg2="y", arg3="n", arg4="n"):
     # This will run the atmos climate model shell bash script "Runmodels.sh"
     # method: use subprocess to run bash in a specific child directory
-    subprocess.run([f"{cwd}/atmos/RunModels.sh", planet_template, "y", "y", "n", "n"],
-                   shell=True, cwd=f"{cwd}/atmos")
+    file = open(f"{cwd}/atmos/RunModels.sh", "r+")
+    lines = file.readlines()
+    # 14, 46, 54, 61, 69
+    lines[14] = f"folder={planet_template}"
+    lines[46] = f" recompile={arg1}"
+    lines[54] = f"  run_photo={arg2}"
+    lines[61] = f"recompile={arg3}"
+    lines[69] = f"run_clima={arg4}"
+    file.close()
+    file = open(f"{cwd}/atmos/RunModels.sh", "r+")
+    file.writelines(lines)
+    file.close()
+
+    subprocess.run([f"{cwd}/atmos/RunModels.sh"], shell=True, cwd=f"{cwd}/atmos")
+
     # Might work differently on different shell environments
 
 
-def generateData(data):
+def generateData(data, module='gru', hidden_dim=50, num_layer=5, iterations=1000, batch_size=30):
     # Use timegan to do deeplearning task of generating new data instances
 
     parameters = dict()
 
-    parameters['module'] = 'gru'
-    parameters['hidden_dim'] = 50
-    parameters['num_layer'] = 5
-    parameters['iterations'] = 1000
-    parameters['batch_size'] = 30
+    parameters['module'] = module
+    parameters['hidden_dim'] = hidden_dim
+    parameters['num_layer'] = num_layer
+    parameters['iterations'] = iterations
+    parameters['batch_size'] = batch_size
     print("Parameters: ", parameters)
     # generate data
     generated_data = timegan(data, parameters)
